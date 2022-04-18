e3e95116eb13ad96c2b4d748392e11bdbb09f06d
wucan1994 <wucan1994@gmail.com>
2022-04-18 17:18:02 +0800

    fix: 关联tab和页面各组件

--- src/components/tab/index.js
+++ src/components/tab/index.js
@@ -3,35 +3,9 @@ import TabBar from './tab-bar';
 import TabPane from './tab-pane';
 import './index.css';
 
-export default function Tab() {
+export default function Tab(props) {
+  const { tabList = [] } = props;
   const [activeIndex, setActiveIndex] = useState(0);
-  const tabList = [
-    {
-      title: '推荐',
-      component: (
-        <div>
-          <span>love</span>
-        </div>
-      ),
-    },
-    {
-      title: '关注',
-      component: (
-        <div>
-          <span>follow</span>
-        </div>
-      ),
-    },
-    {
-      title: '我的',
-      component: (
-        <div>
-          <span>mycenter</span>
-        </div>
-      ),
-    },
-  ];
-
   const tabs = tabList.map((item) => item.title);
 
   const setActiveTab = (index) => {
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
 import Home from './pages/home';
 import Mycenter from './pages/mycenter';
-import Navbar from './components/navbar';
+import Tab from './components/tab';
 import Info from './pages/info';
 import Works from './pages/works';
 import Login from './pages/login';
@@ -14,17 +14,32 @@ import './index.css';
 
 const store = createStore(mycenterReducer);
 
+const tabList = [
+  {
+    title: '推荐',
+    component: <Home />,
+  },
+  {
+    title: '关注',
+    component: <Info />,
+  },
+  {
+    title: '我的',
+    component: <Mycenter />,
+  },
+];
+
 render(
   <Provider store={store}>
     <Router>
-      <Navbar />
+      <Tab tabList={tabList} />
       <div className="content">
-        <Route path="/home" component={Home} />
+        {/* <Route path="/home" component={Home} />
         <Route path="/follow" component={Mycenter} />
         <Route path="/mycenter" component={Mycenter} />
         <Route path="/info" component={Info} />
         <Route path="/works" component={Works} />
-        <Route path="/login" component={Login} />
+        <Route path="/login" component={Login} /> */}
       </div>
     </Router>
   </Provider>,
